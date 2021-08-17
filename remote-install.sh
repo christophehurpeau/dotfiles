06a3202672be4429d656e8042b838d5b7f3ef227
Christophe Hurpeau <christophe@hurpeau.com>
2021-08-17 22:54:57 +0200

    chore: rename install script

--- remote-install.sh
+++ remote-install.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 git clone git@github.com:christophehurpeau/dotfiles.git ~/.dotfiles/ || git clone https://github.com/christophehurpeau/dotfiles.git ~/.dotfiles/
-~/.dotfiles/install.sh
+~/.dotfiles/install
