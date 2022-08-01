89f10583434f7a570de72665ae8d59e1d36301f1
Christophe Hurpeau <christophe@hurpeau.com>
2022-08-01 10:34:44 +0200

    Update remote-install.ps1

--- remote-install.ps1
+++ remote-install.ps1
@@ -1,2 +1,2 @@
-git clone git@github.com:christophehurpeau/dotfiles.git ~/.dotfiles/ || git clone https://github.com/christophehurpeau/dotfiles.git ~/.dotfiles/
+git clone git@github.com:christophehurpeau/dotfiles.git $HOME/.dotfiles/ || git clone https://github.com/christophehurpeau/dotfiles.git $HOME/.dotfiles/
 ~/.dotfiles/install.ps1
