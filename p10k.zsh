55fbc5cafd353c9faab4f28850354d7b92ea6680
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-05-04 14:21:54 +0200

    feat: disable some p10k extensions i do not use

--- p10k.zsh
+++ p10k.zsh
@@ -54,9 +54,9 @@
     # =========================[ Line #2 ]=========================
     newline
     direnv                  # direnv status (https://direnv.net/)
-    asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
-    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
-    anaconda                # conda environment (https://conda.io/)
+    # asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
+    # virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+    # anaconda                # conda environment (https://conda.io/)
     # pyenv                   # python environment (https://github.com/pyenv/pyenv)
     # goenv                   # go environment (https://github.com/syndbg/goenv)
     # nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
